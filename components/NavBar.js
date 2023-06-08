3fbb1c98a02057203911bc856cc68f5dc62546ce
rudsa <rudsa@naver.com>
2023-06-09 00:17:58 +0900

    next-nomad-api

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,20 +6,49 @@ export default function NavBar() {
 
   return (
     <nav>
-      <Link href="/">
-        <span className={router.pathname === "/" ? "active" : ""}>Home</span>
+      <img src="/vercel.svg" />
+      <Link
+        href="/"
+        className={router.pathname === "/" ? "active" : ""}
+        style={{ textDecoration: "none" }}
+      >
+        Home
       </Link>
-      <Link href="/about">
-        <span className={router.pathname === "/about" ? "active" : ""}>
-          About
-        </span>
+      <Link
+        href="/about"
+        className={router.pathname === "/about" ? "active" : ""}
+        style={{ textDecoration: "none" }}
+      >
+        About
       </Link>
       <style jsx>{`
-        a. {
+        Link. {
           text-decoration: none;
+        }
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
         }
         .active {
           color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
