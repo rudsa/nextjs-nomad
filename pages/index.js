1b6e113e68d33496403aeefdf02c3dc643b31cd6
rudsa <rudsa@naver.com>
2023-06-09 18:53:17 +0900

    next-nomad-last

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,7 @@ import Link from "next/link";
 export default function Home({ results }) {
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push(
-      {
-        pathname: `/movies/${id}`,
-        query: {
-          title,
-        },
-      },
-      `/movies/${id}`
-    );
+    router.push(`/movies/${title}/${id}`);
   };
   return (
     <div className="container">
@@ -27,13 +19,7 @@ export default function Home({ results }) {
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>
             <Link
-              href={{
-                pathname: `/movies/${movie.id}`,
-                query: {
-                  title: movie.original_title,
-                },
-              }}
-              as={`/movies/${movie.id}`}
+              href={`/movies/${movie.original_title}/${movie.id}`}
               style={{ textDecoration: "none" }}
             >
               {movie.original_title}
